506e141f33f4eb8c64323305a6e30b837f810e45
Moamaldtu <s195398@student.dtu.dk>
2020-08-09 10:03:26 +0200

    Updated sportAdapter due to errors

--- app/src/main/java/com/fitnessapp2020/beachamp/view/adapter/SportAdapter.kt
+++ app/src/main/java/com/fitnessapp2020/beachamp/view/adapter/SportAdapter.kt
@@ -1,9 +1,23 @@
 package com.fitnessapp2020.beachamp.view.adapter
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.fitnessapp2020.beachamp.model.Sport
 
 class SportAdapter(
     private val items: List<Sport>,
-    private val listener: (Sport, Int) -> Unit) : RecyclerView.Adapter<SportCustomViewHolder> {
+    private val listener: (Sport, Int) -> Unit) : RecyclerView.Adapter<SportCustomViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SportCustomViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: SportCustomViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
