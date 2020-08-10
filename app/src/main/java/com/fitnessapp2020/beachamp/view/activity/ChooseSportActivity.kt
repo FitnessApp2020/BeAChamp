41d7820d82daca95fa003bba115638db5ce72470
Moamaldtu <s195398@student.dtu.dk>
2020-08-10 23:37:33 +0200

    add PlayerInfoActivity to manifest

--- app/src/main/java/com/fitnessapp2020/beachamp/view/activity/ChooseSportActivity.kt
+++ app/src/main/java/com/fitnessapp2020/beachamp/view/activity/ChooseSportActivity.kt
@@ -1,5 +1,6 @@
 package com.fitnessapp2020.beachamp.view.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.View
@@ -9,16 +10,24 @@ import androidx.appcompat.app.AppCompatActivity
 import com.fitnessapp2020.beachamp.R
 import kotlinx.android.synthetic.main.choose_your_sport.*
 
-class ChooseSportActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+class ChooseSportActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener, View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.choose_your_sport)
 
+        // add continue button
+        continue_button_choose.setOnClickListener(this)
+
         val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(this, R.array.sports , android.R.layout.simple_spinner_item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner_choose_sport.adapter = adapter
         spinner_choose_sport.onItemSelectedListener = this
+    }
+
+    override fun onClick(p0: View?) {
+        val intent = Intent(this, PlayerInfoActivity::class.java)
+        startActivity(intent)
     }
 
     override fun onNothingSelected(p0: AdapterView<*>?) {
---
+++ app/src/main/java/com/fitnessapp2020/beachamp/view/activity/PlayerInfoActivity.kt
@@ -0,0 +1,15 @@
+package com.fitnessapp2020.beachamp.view.activity
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.fitnessapp2020.beachamp.R
+import kotlinx.android.synthetic.main.choose_your_sport.*
+
+class PlayerInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.player_info)
+    }
+}
