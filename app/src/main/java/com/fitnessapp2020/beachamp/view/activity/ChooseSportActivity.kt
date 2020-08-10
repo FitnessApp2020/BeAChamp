526b41e0e036b72d01992987090baee1f2f6a54c
Moamaldtu <s195398@student.dtu.dk>
2020-08-10 23:30:58 +0200

    add spinner on choose_your_sport

--- app/src/main/java/com/fitnessapp2020/beachamp/view/activity/ChooseSportActivity.kt
+++ app/src/main/java/com/fitnessapp2020/beachamp/view/activity/ChooseSportActivity.kt
@@ -2,14 +2,30 @@ package com.fitnessapp2020.beachamp.view.activity
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.fitnessapp2020.beachamp.R
+import kotlinx.android.synthetic.main.choose_your_sport.*
 
-class ChooseSportActivity : AppCompatActivity() {
+class ChooseSportActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.choose_your_sport)
 
+        val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(this, R.array.sports , android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner_choose_sport.adapter = adapter
+        spinner_choose_sport.onItemSelectedListener = this
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        println("ho")
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        println("hi")
     }
 }
