fc1bbe24548e29bf5490295283b51b29dc0a5a43
Moamaldtu <s195398@student.dtu.dk>
2020-08-17 11:10:06 +0200

    add ChooseAreaActivity to manifest

---
+++ app/src/main/java/com/fitnessapp2020/beachamp/view/activity/ChooseAreaActivity.kt
@@ -0,0 +1,19 @@
+package com.fitnessapp2020.beachamp.view.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fitnessapp2020.beachamp.R
+
+class ChooseAreaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.choose_areas)
+
+
+
+
+    }
+
+
+}
--- app/src/main/java/com/fitnessapp2020/beachamp/view/activity/PlayerInfoActivity.kt
+++ app/src/main/java/com/fitnessapp2020/beachamp/view/activity/PlayerInfoActivity.kt
@@ -1,15 +1,25 @@
 package com.fitnessapp2020.beachamp.view.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.fitnessapp2020.beachamp.R
-import kotlinx.android.synthetic.main.choose_your_sport.*
+import kotlinx.android.synthetic.main.player_info.*
 
-class PlayerInfoActivity : AppCompatActivity() {
+class PlayerInfoActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.player_info)
+
+        // Add continue button
+        choose_sport_main_button3.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        val intent = Intent(this, ChooseAreaActivity::class.java)
+        startActivity(intent)
     }
 }
