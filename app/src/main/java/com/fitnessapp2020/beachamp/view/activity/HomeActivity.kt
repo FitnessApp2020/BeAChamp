d742f8229bd85d1e25cc309ad2cca9886a559cc1
Momo <s195398@student.dtu.dk>
2020-09-30 21:31:45 +0200

    Add home screen activity

---
+++ app/src/main/java/com/fitnessapp2020/beachamp/view/activity/HomeActivity.kt
@@ -0,0 +1,21 @@
+package com.fitnessapp2020.beachamp.view.activity
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.fitnessapp2020.beachamp.R
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        // Add choose sport button
+        train_button_home.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+    }
+}
