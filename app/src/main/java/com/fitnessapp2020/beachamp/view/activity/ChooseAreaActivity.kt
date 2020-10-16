94cedb8332e853b6afbf9fb956e8b4b83e0bb584
Momo <s195398@student.dtu.dk>
2020-10-16 13:03:12 +0200

    dynamic sport icon added to ChooseAreaActivity

--- app/src/main/java/com/fitnessapp2020/beachamp/view/activity/ChooseAreaActivity.kt
+++ app/src/main/java/com/fitnessapp2020/beachamp/view/activity/ChooseAreaActivity.kt
@@ -5,13 +5,25 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.fitnessapp2020.beachamp.R
+import com.fitnessapp2020.beachamp.model.Athlete
 import kotlinx.android.synthetic.main.choose_areas.*
 
 class ChooseAreaActivity : AppCompatActivity(), View.OnClickListener {
 
+    var athlete = Athlete
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.choose_areas)
+
+        when {
+            athlete.getSport() == "Soccer" -> {
+                imageView_sportIcon_choose.setImageResource(R.drawable.soccer_icon)
+            }
+            athlete.getSport() == "Bodybuilding" -> {
+
+            }
+        }
 
         continue_button_choose_area.setOnClickListener(this)
     }
